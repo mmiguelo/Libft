61bd3de08e0f4c92972a075fc172927c181a664b
Marco Alexandre Miguelote Santos <mmiguelo@c1r9s1.42porto.com>
2024-10-18 18:59:54 +0100

    norms of ft_bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,22 +6,22 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 10:45:58 by mmiguelo          #+#    #+#             */
-/*   Updated: 2024/10/18 18:55:09 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2024/10/18 18:59:08 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	unsigned char	*str;
 	size_t			i;
-	
+
 	if (s == NULL || n == 0)
-		return;
-	str = (unsigned char*) s;
+		return ;
+	str = (unsigned char *) s;
 	i = 0;
-	while(n--)
+	while (n--)
 	{
 		str[i] = '\0';
 		i++;
