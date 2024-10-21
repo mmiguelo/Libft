5d8cc33bc64c39b248d3eebb7375bdd0aae43461
Marco Alexandre Miguelote Santos <mmiguelo@c1r10s4.42porto.com>
2024-10-21 14:29:15 +0100

    substr and makefile

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,43 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 11:44:15 by mmiguelo          #+#    #+#             */
-/*   Updated: 2024/10/21 11:51:19 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2024/10/21 14:04:30 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief extracts a substring from a given string.
+ * 
+ * @param s The original string from which you want to extract a substring.
+ * @param start The starting index from which to begin extraction.
+ * @param len The lenght of the substring.
+ * @return Returns the substring or NULL if the allocation fails.
+ */
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	char	*dest;
+	size_t	i;
+
+	if (!s || start < 0 || len < 0)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (NULL);
+	if (len == 0)
+		return (ft_strdup(""));
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
+	dest = ft_calloc(len + 1, sizeof(char));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s[start] && i < len)
+	{
+		dest[i] = s[start];
+		i++;
+		start++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
