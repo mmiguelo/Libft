b748f31f1ad9484e58e610bdb3a13ea256aeadbe
Marco Alexandre Miguelote Santos <mmiguelo@c1r8s7.42porto.com>
2024-10-30 16:30:32 +0000

    add main to strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 11:47:37 by mmiguelo          #+#    #+#             */
-/*   Updated: 2024/10/23 15:19:07 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2024/10/30 16:30:02 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,3 +41,20 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	str[i] = '\0';
 	return (str);
 }
+
+/* static char	ft_chardown(unsigned int i, char c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (c + 32);
+	return (c);
+}
+int	main(void)
+{
+	char	str[] = "TEST IN THIS";
+	char	*new_str;
+	
+	printf("original string: %s\n", str);
+	new_str = ft_strmapi(str, ft_chardown);
+	printf("Modified string: %s\n", new_str);
+	return(0);
+} */
